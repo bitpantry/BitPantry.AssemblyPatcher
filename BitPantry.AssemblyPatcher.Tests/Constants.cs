bc8283fa879e1bfea84eea32944428ffac0d126f
Jeff Hewitt <jeffhewitt@gmail.com>
2016-01-28 20:29:30 -0600

    Added a compiler directive to help the unit tests find the required resources in AppVeyor

--- BitPantry.AssemblyPatcher.Tests/Constants.cs
+++ BitPantry.AssemblyPatcher.Tests/Constants.cs
@@ -2,7 +2,14 @@
 {
     class Constants
     {
+
+// Use of compiler directive because for some reason AppVeyor can't find the resource under the bin directory
+// not sure if it's getting copied or not
+#if DEBUG
         public const string AssemblyInfoFilePath = @"resources\assemblyInfo.cs.tst";
+#else
+        public const string AssemblyInfoFilePath = @"..\..\resources\assemblyInfo.cs.tst";
+#endif
         public const string TestVersionPartGeneratorToken = "{testGenerator}";
 
         public static readonly string VersionPatternString = string.Format("1.{{#}}.{0}.{1}",
